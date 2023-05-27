8a82158895977efe24b9132cdff46ec94076bfed
Ganga Aloori <ganga.aloori@tide.co>
2023-05-27 12:15:48 +0530

    Pass token to the script

--- modules/config/main.tf
+++ modules/config/main.tf
@@ -33,7 +33,7 @@ resource "rollbar_notification" "slack" {
   }
 
   provisioner "local-exec" {
-    command = "python scripts/rollbar.py ${each.key}"
+    command = "python scripts/rollbar.py ${each.key} ${var.project_api_key}"
   }
 
   depends_on = [rollbar_integration.slack]
